dd5263d9ecc7cadd3207fa7c98fa98c60cee127d
Nbalsh <brandon.bn.nelson@gmail.com>
2014-08-27 17:04:19 -0700

    GLORIOUS START SCREEN thanks Evan :D

--- gen/com/example/drawinggame/R.java
+++ gen/com/example/drawinggame/R.java
@@ -41,8 +41,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int new_pic=0x7f020005;
         public static final int paint=0x7f020006;
         public static final int paint_pressed=0x7f020007;
-        public static final int save=0x7f020008;
-        public static final int small=0x7f020009;
+        public static final int playbuttoncr=0x7f020008;
+        public static final int playbuttonpressedcr=0x7f020009;
+        public static final int save=0x7f02000a;
+        public static final int settingsbuttoncr=0x7f02000b;
+        public static final int settingsbuttonpressedcr=0x7f02000c;
+        public static final int small=0x7f02000d;
+        public static final int titlecr=0x7f02000e;
     }
     public static final class id {
         public static final int draw_btn=0x7f090001;
